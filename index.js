81c368719a26e18b12bc6812f7331788e83e4c26
thibault durand <tech@storylinr.net>
2018-05-26 18:42:19 +0200

    new test

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ const client = dgram.createSocket('udp4');
 app.post('/event_cell', function (req, res) {
  res.send('Ok');
  //client.send(message, 41234, '127.0.0.1', (err) => {
- console.log(err);
-});
-
+ //console.log(err);
+ //});
+})
 app.get('/event_cell', function (req, res) {
  res.send('Ok');
  //client.send(message, 41234, '127.0.0.1', (err) => {
- console.log(err);
-});
+ //console.log(err);
+ //});
 
 })
 
