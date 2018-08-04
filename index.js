2d29b8f567a03be7e46068ddbc57626ddbad7463
thibault <thibault@realtime-art.net>
2018-08-04 19:57:55 +0200

    ws

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Creating WebSocket server
-const wss = new WebSocket.Server({ server: app });
+const wss = new WebSocket.Server({ port: 41234 });
 
 wss.on('connection', function connection(ws) {
 	clients.push (ws);
